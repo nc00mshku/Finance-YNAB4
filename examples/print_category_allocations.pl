7a600c385d9c9e4e624cc1c019d5505f69afe0ec
nc00mshku <nc00mshku@pastywhitesoftware.com>
2016-05-09 22:32:01 -0500

    fix warning in example script

--- examples/print_category_allocations.pl
+++ examples/print_category_allocations.pl
@@ -11,12 +11,14 @@ my $ynab4 = Finance::YNAB4->new(budget_file => $budget);
 my $budeted_months = $ynab4->get_budget_ids;
 
 my $budgets = $ynab4->get_budgeted;
+
 for my $b_month (@$budeted_months) {
     print "[$b_month]\n";
-    for my $cat_id (keys $budgets->{$b_month}) {
+    for my $cat_id (keys %{$budgets->{$b_month}}) {
 	my $category_name = $ynab4->get_category_name($cat_id);
 	next unless $category_name;
-	print "\t".join(',',($category_name,$budgets->{$b_month}->{$cat_id}))."\n";
+	
+	print "\t".join(',',($cat_id,$category_name,$budgets->{$b_month}->{$cat_id}))."\n";
     }
 }
 
